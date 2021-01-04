2d52b61c68db994899cf4ba5db8683c4ce9ae067
Caleb L. Power <cpower@axonibyte.com>
2021-01-04 01:36:08 -0600

    cleanup + docs

--- src/main/java/com/calebpower/crypto/bc/BCP2PTestDriver.java
+++ src/main/java/com/calebpower/crypto/bc/BCP2PTestDriver.java
@@ -2,23 +2,30 @@ package com.calebpower.crypto.bc;
 
 import org.bouncycastle.util.encoders.Base64;
 
+/**
+ * Test driver for various Bouncy Castle algorithms, the purpose of which is to
+ * serve as a proof-of-concept for automatic generation of various keys and the
+ * sharing of per-document symmetric keys.
+ * 
+ * @author Caleb L. Power
+ */
 public class BCP2PTestDriver {
   
-  public static void main(String[] args) {
+  /**
+   * Entry points.
+   * 
+   * @param args program arguments
+   */
+  public static void main(String... args) {
     System.out.println("Hello, world!");
     
     try {
       String plaintext = "This is my really cool plaintext.";
-      // String key = "01234567890123456789012345678901"; // needs to be 256 bits
-      //String iv = "1234567891234567"; // 128 bits
       
       SymmetricEngine symmetricEngine = new SymmetricEngine();
       String key = symmetricEngine.genKey();
       
       System.out.printf("Plaintext: %1$s\nKey: %2$s\n", plaintext, key);
-      
-      //SymmetricEngine symmetricEngine = new SymmetricEngine(key, iv);
-      //SymmetricEngine symmetricEngine2 = new SymmetricEngine(key, iv);
       
       System.out.println("Key size = " + (Base64.decode(key).length * 8));
       
@@ -28,5 +35,18 @@ public class BCP2PTestDriver {
     } catch(Exception e) {
       e.printStackTrace();
     }
+  }
+
+  /**
+   * Utility to print a byte array in hex;
+   * 
+   * @param bytes the byte array
+   */
+  public static void printBytes(byte[] bytes) {
+    for(int i = 0; i < bytes.length; i++) {
+      System.out.printf("%02X ", bytes[i]);
+      if((i + 1) % 16 == 0) System.out.println();
+    }
+    System.out.println();
   }
 }
--- src/main/java/com/calebpower/crypto/bc/SymmetricEngine.java
+++ src/main/java/com/calebpower/crypto/bc/SymmetricEngine.java
@@ -11,6 +11,16 @@ import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.crypto.params.ParametersWithIV;
 import org.bouncycastle.util.encoders.Base64;
 
+/**
+ * Encrypts and decrypts messages with a symmetric key.
+ * Generates said key, and the appropriate IV.
+ * 
+ * - Algorithm: AES 256
+ * - Block Cipher: CBC
+ * - Padding: PKCS7
+ * 
+ * @author Caleb L. Power
+ */
 public class SymmetricEngine {
   
   private static final int BLOCK_SIZE = 16;
@@ -20,50 +30,66 @@ public class SymmetricEngine {
   private PaddedBufferedBlockCipher cipher = new PaddedBufferedBlockCipher(
       new CBCBlockCipher(new AESEngine()),
       new PKCS7Padding());
-  // private ParametersWithIV keyParamWithIV = null;
   
-  /*
-  public SymmetricEngine(String key, String iv) throws Exception {
-    byte[] keyBytes = key.getBytes("UTF-8");
-    byte[] saltBytes = new byte[0];
-    // PKCS5S2ParametersGenerator generator = new PKCS5S2ParametersGenerator();
-    // generator.init(keyBytes, saltBytes, 4096);
-    // keyParamWithIV = (ParametersWithIV)generator.generateDerivedParameters(256, 128);
-    // byte[] derivedKey = ((KeyParameter)generator.generateDerivedParameters(256, 128)).getKey();
-    
-    // keyParamWithIV = new ParametersWithIV(new KeyParameter(key.getBytes("UTF-8")), iv.getBytes("UTF-8"), 0, 16);
-  }
-  */
-  
+  /**
+   * Generates securely pseudorandom 256-bit key.
+   * 
+   * @return a Base64-encoded 256-bit key
+   * @throws NoSuchAlgorithmException thrown if the SecureRandom strong instance can't be found
+   */
   public String genKey() throws NoSuchAlgorithmException {
     byte[] bytes = new byte[KEY_SIZE];
     SecureRandom.getInstanceStrong().nextBytes(bytes);
     return new String(Base64.encode(bytes));
   }
   
+  /**
+   * Encrypts plaintext using a 256-bit key.
+   * 
+   * @param plaintext the plaintext
+   * @param key the 256-bit key
+   * @return Base64-encoded ciphertext preceded by the IV
+   * @throws Exception if something went wrong
+   */
   public String encrypt(String plaintext, String key) throws Exception {
+    byte[] keyBytes = Base64.decode(key); // decode
+    
+    // must be exactly 256 bits; consider PBKDF2 (PKCS5S2) for key expansion algorithms if keys aren't auto-generated
+    if(keyBytes.length != KEY_SIZE) throw new Exception("Bad key length.");
+    
     byte[] iv = new byte[BLOCK_SIZE];
-    SecureRandom.getInstanceStrong().nextBytes(iv);
+    SecureRandom.getInstanceStrong().nextBytes(iv); // generate a random initial vector
+    
     ParametersWithIV keyParamWithIV = new ParametersWithIV(new KeyParameter(Base64.decode(key)), iv);
     
     cipher.init(true, keyParamWithIV);
     byte[] plaintextBytes = plaintext.getBytes(CHARSET);
+    
+    // the ciphertext is going to have the IV prepended to it
     byte[] ciphertextBytes = new byte[cipher.getOutputSize(plaintextBytes.length) + BLOCK_SIZE];
     
-    printBytes(ciphertextBytes);
+    BCP2PTestDriver.printBytes(ciphertextBytes); // XXX prints the blank array for confirmation
     
     for(int i = 0; i < BLOCK_SIZE; i++) ciphertextBytes[i] = iv[i];
     
-    printBytes(ciphertextBytes);
+    BCP2PTestDriver.printBytes(ciphertextBytes); // XXX prints the array with the only an IV
     
     cipher.doFinal(ciphertextBytes,
         cipher.processBytes(plaintextBytes, 0, plaintextBytes.length, ciphertextBytes, BLOCK_SIZE) + BLOCK_SIZE);
     
-    printBytes(ciphertextBytes);
+    BCP2PTestDriver.printBytes(ciphertextBytes); // XXX prints the array with the appended encryption
     
-    return new String(Base64.encode(ciphertextBytes));
+    return new String(Base64.encode(ciphertextBytes)); // returns Base64-encoded ciphertext
   }
   
+  /**
+   * Decrypts ciphertext using a 256-bit key.
+   * 
+   * @param ciphertext the ciphertext, with a 128-bit IV prepended to it
+   * @param key the 256-bit key
+   * @return the plaintext
+   * @throws Exception if something went wrong
+   */
   public String decrypt(String ciphertext, String key) throws Exception {
     byte[] ciphertextBytes = Base64.decode(ciphertext);
 
@@ -74,14 +100,6 @@ public class SymmetricEngine {
     cipher.doFinal(plaintextBytes,
         cipher.processBytes(ciphertextBytes, BLOCK_SIZE, ciphertextBytes.length - BLOCK_SIZE, plaintextBytes, 0));
     return new String(plaintextBytes, CHARSET);
-  }
-  
-  public void printBytes(byte[] bytes) {
-    for(int i = 0; i < bytes.length; i++) {
-      System.out.printf("%02X ", bytes[i]);
-      if((i + 1) % 16 == 0) System.out.println();
-    }
-    System.out.println();
   }
   
 }
